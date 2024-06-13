52f1f756a8d93e3a0d565806997de59b93b8fb3e
Lucas Diaz <lucasdm0905@gmail.com>
2024-06-13 08:06:12 -0300

    Singleton
    Signed-off-by: Lucas Diaz <lucasdm0905@gmail.com>

---
+++ src/gof/Test.java
@@ -0,0 +1,19 @@
+public class Test {
+    
+    public static void main(String[] args) {
+        SingletonLazy lazy = SingletonLazy.getInstancia();
+        System.out.println(lazy);
+        lazy = SingletonLazy.getInstancia();
+        System.out.println(lazy);
+
+        SingletonEager eager = SingletonEager.getInstancia();
+        System.out.println(eager);
+        eager = SingletonEager.getInstancia();
+        System.out.println(eager);
+
+        SingletonLazyHolder Holder = SingletonLazyHolder.getInstancia();
+        System.out.println(Holder);
+        Holder = SingletonLazyHolder.getInstancia();
+        System.out.println(Holder);
+    }
+}
